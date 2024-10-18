29b2fe6a36d0ae2fcf608832b5b3be05756a6cf5
alexay7 <43906716+alexay7@users.noreply.github.com>
2024-10-18 19:12:16 +0200

    Docker things

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,4 +10,13 @@ export default defineConfig({
             "@": path.resolve(__dirname, "./src"),
         },
     },
+    server:{
+        host: true,
+        port:3000,
+        watch:{
+            usePolling:true
+        },
+        strictPort:true,
+        origin:"https://polls.manabe.es"
+    }
 });
