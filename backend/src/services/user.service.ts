eb7274315c931b682c3934612e9375cec4ad6c8a
alexay7 <43906716+alexay7@users.noreply.github.com>
2025-01-04 18:12:12 +0100

    Limit tickets to 5

--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -2,7 +2,15 @@ import { User } from "../models/user.model";
 
 export const userService = {
     incrementUserTickets: async (userId:string) => {
-        return User.findOneAndUpdate({userId}, {$inc: {tickets: 1}}, {new: true, upsert:true});
+        const foundUser = await User.findOne({userId});
+
+        if (!foundUser) {
+            return User.create({userId,tickets:1});
+        } else if (foundUser.tickets <5){
+            return User.findOneAndUpdate({userId}, {$inc: {tickets: 1}}, {new: true});
+        }else{
+            return foundUser;
+        }
     },
     decrementUserTickets: async (userId:string) => {
         const foundUser = await User.findOne({userId});
