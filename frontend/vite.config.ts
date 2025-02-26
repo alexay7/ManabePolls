2cd089c2756c40d8389ad28de71f8cf2616c195b
alexay7 <43906716+alexay7@users.noreply.github.com>
2025-02-27 00:16:30 +0100

    Testing if gordoz is wrong

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,35 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import {defineConfig, loadEnv} from "vite";
 import svgr from "vite-plugin-svgr";
 
-export default defineConfig({
-    plugins: [react(), svgr()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
+export default ({mode}: { mode: string }
+) => {
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+    return defineConfig({
+        plugins: [react(), svgr()],
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "./src"),
+            },
         },
-    },
-    server:{
-        host: true,
-        port:3000,
-        watch:{
-            usePolling:true
-        },
-        strictPort:true,
-        origin:"https://polls.manabe.es"
-    }
-});
+        server: {
+            host: true,
+            port: 3000,
+            watch: {
+                usePolling: true
+            },
+            strictPort: true,
+            origin: "https://polls.manabe.es",
+            proxy: {
+                "/api": {
+                    target: process.env.VITE_APP_API_URL,
+                    changeOrigin: true,
+                    secure: false,
+                    rewrite: (path) => path.replace(/^\/api/, ""),
+                }
+            }
+        }
+    });
+};
